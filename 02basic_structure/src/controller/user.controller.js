c9b7532170cdfd5ddbded93a37f42a9b85bfbad3
Ashish Jangde <ashishjangde2003@gmail.com>
2024-10-27 21:12:38 +0530

    added signin and signup route

--- 02basic_structure/src/controller/user.controller.js
+++ 02basic_structure/src/controller/user.controller.js
@@ -2,72 +2,156 @@ import User from "../models/user.model.js";
 import { APIResponse } from "../utils/ApiResponse.js";
 import { APIError } from "../utils/ApiError.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 
 
 
 export const SignUp = async (req, res) => {
     try {
-        const {name,email,username,password,age} = req.body;   
-        console.log( req.body);
-       // check user pehle exist hai ya nahi
-     const existingUser = await User.findOne({email});
-     console.log(existingUser);
+        // requirement
+        const { name, username, email, password, age } = req.body;
 
-     if(existingUser != null){
-        return res.status(400).json({
-           apiError: new APIResponse(new APIError(400,"user already exist")),
-        })
-     }
+        // field verification
+        if (!name || !username || !email || !password) {
+            return res.status(400).json({
+                data: new APIResponse(null, new APIError(400, "All fields are required"))    // data  //
+            });
+        }
+        // check if user already exists
+        const existingUser = await User.findOne({
+            email,
+            isverified: true
+        });
 
-     const hashedPassword = await bcrypt.hash(password, 10);
-      // password hash karke save karna hai
+        if (existingUser) {
+            return res.status(400).json({
+                data: new APIResponse(null, new APIError(400, "User already exists"))    // data  //
+            });
+        }
 
-     const newUser = new User({
-        name,
-        email,
-        username,
-        password : hashedPassword,
-        age,
-     });
+        // hashing password
+        const hashedPassword = await bcrypt.hash(password, 10) //salt rounds -> 10 rounds 
 
+        // verification code generation
+        const verificationCode = Math.floor(Math.random() * 900000) + 100000;
+
+        const verificationCodeExpiry = new Date(Date.now() + 10 * 60 * 1000);  // 10 minutes
+
+        // create new user 
+        const newUser = new User({
+            name,
+            username,
+            email,
+            password: hashedPassword,
+            age,
+            verificationCode,
+            verificationCodeExpiry
+        });
+
+        // send verification code
+        // send email  --> send email to user
+
+        // save user to database
         await newUser.save();
 
-     const savedUser = await User.findOne({email}).select("-password - __V");
+        return res.status(201).json({
+            data: new APIResponse(newUser)
+        });
 
-     console.log(savedUser);
-        
-
-       return res.status(201).json({  // 201 created
-          data : new APIResponse(savedUser),
-        })
-
-        
 
     } catch (error) {
-        const response = new APIResponse(null ,new APIError(500,error));  // default system error
-            console.error(); // Log the error for debugging
-            return res.status(500).json({
-                response
-            });
+
+        return res.status(500).json({
+            data: new APIResponse(null, new APIError(500, error))
+        });
     }
 }
 
 export const SignIn = async (req, res) => {
 
     try {
-        const {email, password} = req.body;
+        // requirements
+        const { email, password } = req.body;
 
-        console.log(req.body);
+        // field verification
+        if (!email || !password) {
+            return res.status(400).json({
+                data: new APIResponse(null, new APIError(400, "All fields are required"))    // data  //
+            });
+        }
+
+        // check if user exists
+        const existingUser = await User.findOne({
+            email,
+            isverified: true
+        });
+
+        if (!existingUser) {
+            return res.status(400).json({
+                data: new APIResponse(null, new APIError(400, "User not found"))    // data  //
+            });
+        }
+
+        // check if password is correct
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password); // normal ko hash karega fir companre karega  or return karega true false
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                data: new APIResponse(null, new APIError(400, "Password is incorrect"))    // data  //    
+            });
+        }
+
+        // generate token
+        const accessToken = jwt.sign(
+            {
+                id: existingUser._id,
+                email: existingUser.email
+            },
+            process.env.JWTTOKENSECRET,
+            {
+                expiresIn: "10m"
+            });
+
+        const refreshToken = jwt.sign(
+            {
+                id: existingUser._id,
+                email: existingUser.email
+            },
+            process.env.REFRESHTOKENSECRET,
+            {
+                expiresIn: "1y"
+            });
+
+            // ek token ko apne pass save kar lia
+            existingUser.token = refreshToken;   //satabase me store kara lia ek token 
+            
+            // save the user
+            existingUser.save();
+
+            const userTobeSent =  User.findOne({email}).select("-password -token -verificationCode -verificationCodeExpiry");
+           
+            res.status(200)
+            .cookie("refreshToken", refreshToken, {
+                httpOnly: true,   // ko access karne ke liye // backend acess only
+                path: "/",  // kis path me access karne ke liye
+            })
+            .json({
+                data: new APIResponse(userTobeSent, accessToken)
+            });
 
     } catch (error) {
-        
+        return res.status(500).json({
+            data: new APIResponse(null, new APIError(500, error))
+        });
+
     }
 
 }
 
 
+
 // {
- //"email": "a@a.com",
- //"password": "ashish@123",
+//"email": "a@a.com",
+//"password": "ashish@123",
 //}
--- 02basic_structure/src/models/user.model.js
+++ 02basic_structure/src/models/user.model.js
@@ -16,14 +16,31 @@ const userSchema = new mongoose.Schema({
         required:[true,"username is required"],
         unique:true
     },
+    isverified:{
+        type:Boolean,
+        default:false
+    },
     password:{
         type:String,
         required:[true,"password is required"],
     },
+    verificationCode:{
+      type:String,
+      required:false 
+    },
+    verificationCodeExpiry:{
+        type:Date,
+        required:false
+        
+    },
+    token:{
+        type:String,
+        required:false
+    },
     age:{
         type:Number,
         min:[18,"age must be greater than 18"],
-        max:[50,"age must be less than 100"]
+        max:[70,"age must be less than 100"]
     }
 
 },{timestamps:true})  // created at and updated at
